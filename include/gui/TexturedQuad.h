e98c819dfe1774f582e0a57df42e435088df5bac
Zalasus <niklas.weissner@gmail.com>
2018-06-22 12:31:54 +0200

    Add TexturedQuad class

---
+++ include/gui/TexturedQuad.h
@@ -0,0 +1,51 @@
+/*
+ * TexturedQuad.h
+ *
+ *  Created on: 22 Jun 2018
+ *      Author: zal
+ */
+
+#ifndef INCLUDE_GUI_TEXTUREDQUAD_H_
+#define INCLUDE_GUI_TEXTUREDQUAD_H_
+
+#include <osg/Geometry>
+
+#include "db/Texture.h"
+
+namespace od
+{
+
+    /**
+     * @brief Dynamic, flat, textured quad for GUI stuff.
+     *
+     * This is a osg::Geometry for representing a rectangle defined by it's top left and bottom right corners.
+     * It is always parallel to the xy-plane. The z component can be specified, but it is shared by all four vertices.
+     */
+    class TexturedQuad : public osg::Geometry
+    {
+    public:
+
+        TexturedQuad();
+        TexturedQuad(const TexturedQuad& geometry, const osg::CopyOp &copyop = osg::CopyOp::SHALLOW_COPY);
+
+        META_Node(od, TexturedQuad);
+
+        void setTextureCoords(const osg::Vec2 &topLeft, const osg::Vec2 &bottomRight);
+        void setTexture(Texture *texture);
+        void setVertexCoords(const osg::Vec2 &topLeft, const osg::Vec2 &bottomRight);
+        void setZCoord(float z);
+
+
+    private:
+
+        osg::ref_ptr<osg::Vec3Array> mVertexArray;
+        osg::ref_ptr<osg::Vec2Array> mTextureCoordArray;
+        osg::ref_ptr<osg::Vec4Array> mColorArray;
+        osg::ref_ptr<osg::Texture2D> mTexture;
+        osg::ref_ptr<Texture> mTextureAsset;
+        float mZCoord;
+    };
+
+}
+
+#endif /* INCLUDE_GUI_TEXTUREDQUAD_H_ */
---
+++ src/gui/TexturedQuad.cpp
@@ -0,0 +1,80 @@
+/*
+ * TexturedQuad.cpp
+ *
+ *  Created on: 22 Jun 2018
+ *      Author: zal
+ */
+
+#include "gui/TexturedQuad.h"
+
+#include <osg/Array>
+
+namespace od
+{
+
+    TexturedQuad::TexturedQuad()
+    : mVertexArray(new osg::Vec3Array(4))
+    , mTextureCoordArray(new osg::Vec2Array(4))
+    , mColorArray(new osg::Vec4Array(4))
+    , mTexture(new osg::Texture2D)
+    , mZCoord(1)
+    {
+        this->setVertexArray(mVertexArray);
+        this->setTexCoordArray(0, mTextureCoordArray, osg::Array::BIND_PER_VERTEX);
+        this->setColorArray(mColorArray, osg::Array::BIND_PER_VERTEX);
+
+        this->getOrCreateStateSet()->setTextureAttribute(0, mTexture);
+
+        this->setDataVariance(osg::Object::DYNAMIC);
+        this->setUseDisplayList(false);
+        this->setUseVertexBufferObjects(true);
+    }
+
+    TexturedQuad::TexturedQuad(const TexturedQuad& quad, const osg::CopyOp &copyop)
+    : Geometry(quad, copyop)
+    , mZCoord(quad.mZCoord)
+    {
+        mVertexArray = static_cast<osg::Vec3Array*>(this->getVertexArray());
+        mTextureCoordArray = static_cast<osg::Vec2Array*>(this->getTexCoordArray(0));
+        mColorArray = static_cast<osg::Vec4Array*>(this->getColorArray());
+        mTexture = static_cast<osg::Texture2D*>(this->getOrCreateStateSet()->getTextureAttribute(0, osg::StateAttribute::TEXTURE));
+    }
+
+    void TexturedQuad::setTextureCoords(const osg::Vec2 &topLeft, const osg::Vec2 &bottomRight)
+    {
+        mTextureCoordArray->at(0) = topLeft;
+        mTextureCoordArray->at(1) = osg::Vec2(bottomRight.x(), topLeft.y());
+        mTextureCoordArray->at(2) = osg::Vec2(topLeft.x(), bottomRight.y());
+        mTextureCoordArray->at(3) = bottomRight;
+    }
+
+    void TexturedQuad::setTexture(Texture *texture)
+    {
+        mTextureAsset = texture;
+        mTexture->setImage(texture);
+    }
+
+    void TexturedQuad::setVertexCoords(const osg::Vec2 &topLeft, const osg::Vec2 &bottomRight)
+    {
+        mVertexArray->at(0) = osg::Vec3(topLeft, mZCoord);
+        mVertexArray->at(1) = osg::Vec3(bottomRight.x(), topLeft.y(), mZCoord);
+        mVertexArray->at(2) = osg::Vec3(topLeft.x(), bottomRight.y(), mZCoord);
+        mVertexArray->at(3) = osg::Vec3(bottomRight, mZCoord);
+
+        this->dirtyBound();
+    }
+
+    void TexturedQuad::setZCoord(float z)
+    {
+        mZCoord = z;
+
+        mVertexArray->at(0).z() = z;
+        mVertexArray->at(1).z() = z;
+        mVertexArray->at(2).z() = z;
+        mVertexArray->at(3).z() = z;
+
+        this->dirtyBound();
+    }
+
+
+}
