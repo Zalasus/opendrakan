edb7a95fc4dce09a69d3a53b1ec7d0799f29edc5
Zalasus <niklas.weissner@gmail.com>
2018-09-10 14:09:03 +0200

    Document LightState classes

--- include/light/LightState.h
+++ include/light/LightState.h
@@ -18,12 +18,14 @@ namespace od
 
     class LightManager;
 
-
+    /**
+     * @brief A StateAttribute handling a list of lights used internally by LightStateCallback.
+     *
+     * @note This should not be used directly. Use a LightStateCallback instead.
+     */
     class LightStateAttribute : public osg::StateAttribute
     {
     public:
-
-        friend class LightStateCallback;
 
         LightStateAttribute();
         LightStateAttribute(const LightStateAttribute &l, const osg::CopyOp &copyOp = osg::CopyOp::SHALLOW_COPY);
@@ -52,10 +54,32 @@ namespace od
     };
 
 
+    /**
+     * @brief A cull callback handling lighting.
+     *
+     * This callback handles applying the closest lights to a node when necessary (since in
+     * forward rendering, the maximum number of lights in global state is limited).
+     *
+     * A fixed light can be defined that is always applied. This will most likely always be the layer light
+     * associated with the object or layer.
+     */
     class LightStateCallback : public osg::NodeCallback
     {
     public:
 
+        /**
+         * @brief Constructor for a LightStateCallback
+         *
+         * Creates a LightStateAttribute and attaches it to \c node.
+         * The passed node is not necessarily the one the callback is attached to. It should be
+         * a unique node that will have the same modelview matrix as the geometry to be lighted.
+         *
+         * If the passed node is shared among multiple LightStateCallback, the resulting light
+         * behaviour is undefined.
+         *
+         * @param  lm    A LightManager to fetch lights from when updating light state
+         * @param  node  The node on which the light state should be applied
+         */
         LightStateCallback(LightManager &lm, osg::Node *node);
 
         inline void lightingDirty() { mLightingDirty = true; }
