86560eba861ac3a7ec6680e3ac8046f71a2ccbac
zalasus <niklas.weissner@gmail.com>
2018-12-03 09:43:39 +0100

    Add Skeleton and build method

--- include/odCore/db/Skeleton.h
+++ include/odCore/db/Skeleton.h
@@ -11,26 +11,80 @@
 #include <string>
 #include <vector>
 #include <glm/mat4x4.hpp>
+#include <glm/mat3x4.hpp>
 
 namespace odDb
 {
-
 
     class Skeleton
     {
     public:
 
-        void addNode(const std::string &name, int32_t jointInfoIndex);
-        void addJointInfo(const glm::mat4 &boneXform, int32_t meshIndex, int32_t firstChildIndex, int32_t nextSiblingIndex);
-        void markJointAsChannel(size_t jointIndex);
+        class Bone
+        {
+        public:
+
+            explicit Bone(Skeleton &skeleton);
+            Bone(const Bone &bone);
+
+            inline const std::string &getName() const { return mName; }
+            inline const glm::mat4 &getInverseBinPoseTransform() const { return mInverseBindPoseTransform; }
+            inline void setName(const std::string &name) { mName = name; }
+            inline void setInverseBindPoseTransform(const glm::mat4 &tform) { mInverseBindPoseTransform = tform; }
+
+            size_t getChildBoneCount();
+            Bone *getChildBone(size_t index);
+            Bone *addChildBone();
+
+            void moveToBindPose();
+
+
+        private:
+
+            Skeleton &mSkeleton;
+            std::string mName;
+            glm::mat4 mInverseBindPoseTransform;
+            std::vector<Bone*> mChildBones;
+
+            glm::mat4 mCurrentMatrix;
+        };
+
+        friend class Bone;
+
+        explicit Skeleton(size_t initialBoneCapacity = 0);
+        Skeleton(const Skeleton &skeleton);
+
+        Bone *addRootBone();
 
 
     private:
 
-        struct NodeInfo
+        std::vector<Bone> mBones;
+
+    };
+
+
+    class SkeletonBuilder
+    {
+    public:
+
+        SkeletonBuilder();
+
+        inline size_t getJointCount() const { return mJointInfos.size(); }
+
+        void addJointNameInfo(const std::string &name, int32_t jointIndex);
+        void addJointInfo(const glm::mat4 &boneXform, int32_t meshIndex, int32_t firstChildIndex, int32_t nextSiblingIndex);
+        void markJointAsChannel(size_t jointIndex);
+
+        void build(Skeleton &skeleton);
+
+
+    private:
+
+        struct JointNameInfo
         {
             std::string name;
-            int32_t jointInfoIndex;
+            int32_t jointIndex;
         };
 
         struct JointInfo
@@ -39,11 +93,18 @@ namespace odDb
             int32_t meshIndex;
             int32_t firstChildIndex;
             int32_t nextSiblingIndex;
+
+            JointNameInfo *nameInfo;
             bool isChannel;
+
+            bool visited;
         };
 
-        std::vector<NodeInfo> mNodes;
-        std::vector<JointInfo> mJoints;
+        void _buildRecursive(Skeleton::Bone *parent, JointInfo *jointInfo);
+
+        std::vector<JointNameInfo> mNameInfos;
+        std::vector<JointInfo> mJointInfos;
+        bool mAlreadyBuiltNameLinks;
     };
 
 }
--- src/odCore/db/Skeleton.cpp
+++ src/odCore/db/Skeleton.cpp
@@ -14,16 +14,82 @@
 namespace odDb
 {
 
-    void Skeleton::addNode(const std::string &name, int32_t jointInfoIndex)
+    Skeleton::Bone::Bone(Skeleton &skeleton)
+    : mSkeleton(skeleton)
     {
-        NodeInfo info;
-        info.name = name;
-        info.jointInfoIndex = jointInfoIndex;
-
-        mNodes.push_back(info);
     }
 
-    void Skeleton::addJointInfo(const glm::mat4 &boneXform, int32_t meshIndex, int32_t firstChildIndex, int32_t nextSiblingIndex)
+    Skeleton::Bone::Bone(const Bone &bone)
+    : mSkeleton(bone.mSkeleton)
+    , mName(bone.mName)
+    , mInverseBindPoseTransform(bone.mInverseBindPoseTransform)
+    , mChildBones(bone.mChildBones)
+    , mCurrentMatrix(mInverseBindPoseTransform)
+    {
+    }
+
+    size_t Skeleton::Bone::getChildBoneCount()
+    {
+        return mChildBones.size();
+    }
+
+    Skeleton::Bone *Skeleton::Bone::getChildBone(size_t index)
+    {
+        if(index >= mChildBones.size())
+        {
+            throw od::Exception("Child bone index out of bounds");
+        }
+
+        return mChildBones[index];
+    }
+
+    Skeleton::Bone *Skeleton::Bone::addChildBone()
+    {
+        mSkeleton.mBones.push_back(Bone(mSkeleton));
+        mChildBones.push_back(&mSkeleton.mBones.back());
+        return mChildBones.back();
+    }
+
+    void Skeleton::Bone::moveToBindPose()
+    {
+        mCurrentMatrix = glm::inverse(mInverseBindPoseTransform);
+    }
+
+
+    Skeleton::Skeleton(size_t initialBoneCapacity)
+    {
+        mBones.reserve(initialBoneCapacity);
+    }
+
+    Skeleton::Skeleton(const Skeleton &skeleton)
+    : mBones(skeleton.mBones)
+    {
+    }
+
+    Skeleton::Bone *Skeleton::addRootBone()
+    {
+        mBones.push_back(Bone(*this));
+
+        return &mBones.back();
+    }
+
+
+
+    SkeletonBuilder::SkeletonBuilder()
+    : mAlreadyBuiltNameLinks(false)
+    {
+    }
+
+    void SkeletonBuilder::addJointNameInfo(const std::string &name, int32_t jointIndex)
+    {
+        JointNameInfo info;
+        info.name = name;
+        info.jointIndex = jointIndex;
+
+        mNameInfos.push_back(info);
+    }
+
+    void SkeletonBuilder::addJointInfo(const glm::mat4 &boneXform, int32_t meshIndex, int32_t firstChildIndex, int32_t nextSiblingIndex)
     {
         JointInfo info;
         info.boneXform = boneXform;
@@ -31,17 +97,98 @@ namespace odDb
         info.firstChildIndex = firstChildIndex;
         info.nextSiblingIndex = nextSiblingIndex;
 
-        mJoints.push_back(info);
+        mJointInfos.push_back(info);
     }
 
-    void Skeleton::markJointAsChannel(size_t jointIndex)
+    void SkeletonBuilder::markJointAsChannel(size_t jointIndex)
     {
-        if(jointIndex >= mJoints.size())
+        if(jointIndex >= mJointInfos.size())
         {
             throw od::Exception("Channel index in skeleton out of joint index range");
         }
 
-        mJoints[jointIndex].isChannel = true;
+        mJointInfos[jointIndex].isChannel = true;
+    }
+
+    void SkeletonBuilder::build(Skeleton &skeleton)
+    {
+        if(!mAlreadyBuiltNameLinks)
+        {
+            for(auto it = mNameInfos.begin(); it != mNameInfos.end(); ++it)
+            {
+                if(it->jointIndex < 0)
+                {
+                    continue;
+
+                }else if(it->jointIndex >= mJointInfos.size())
+                {
+                    throw od::Exception("Joint name info's referenced joint index is out of bounds");
+                }
+
+                mJointInfos[it->jointIndex].nameInfo = &(*it);
+            }
+
+            mAlreadyBuiltNameLinks = true;
+        }
+
+        // by iterating over all joints and starting to build from all untouched joints we encounter, we can
+        //  also handle skeletons that have more than one root for some reason
+        for(auto it = mJointInfos.begin(); it != mJointInfos.end(); ++it)
+        {
+            if(it->visited)
+            {
+                continue;
+            }
+
+            Skeleton::Bone *root = skeleton.addRootBone();
+            _buildRecursive(root, &(*it));
+        }
+    }
+
+    void SkeletonBuilder::_buildRecursive(Skeleton::Bone *parent, JointInfo *jointInfo)
+    {
+        if(parent == nullptr || jointInfo == nullptr)
+        {
+            return;
+        }
+
+        if(jointInfo->visited)
+        {
+            throw od::Exception("Encountered loop in bone tree");
+
+        }else
+        {
+            jointInfo->visited = true;
+        }
+
+        Skeleton::Bone *bone = parent->addChildBone();
+        bone->setInverseBindPoseTransform(jointInfo->boneXform);
+        if(jointInfo->nameInfo != nullptr)
+        {
+            bone->setName(jointInfo->nameInfo->name);
+        }
+
+        if(jointInfo->firstChildIndex > 0)
+        {
+            if(jointInfo->firstChildIndex >= (int32_t)mJointInfos.size())
+            {
+                throw od::Exception("First child index in joint info out of bounds");
+            }
+
+            JointInfo *firstChildJointInfo = &mJointInfos[jointInfo->firstChildIndex];
+            _buildRecursive(bone, firstChildJointInfo);
+        }
+
+        if(jointInfo->nextSiblingIndex > 0)
+        {
+            if(jointInfo->nextSiblingIndex >= (int32_t)mJointInfos.size())
+            {
+                throw od::Exception("Next sibling index in joint info out of bounds");
+            }
+
+            JointInfo *nextSiblingJointInfo = &mJointInfos[jointInfo->nextSiblingIndex];
+            _buildRecursive(parent, nextSiblingJointInfo);
+        }
     }
 
 }
