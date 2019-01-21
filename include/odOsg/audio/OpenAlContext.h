792ca32c7ce235ae909bf32a4f647c716dfed9e2
zalasus <niklas.weissner@gmail.com>
2019-01-21 09:35:28 +0100

    Add OpenAL context wrapper

---
+++ include/odOsg/audio/OpenAlContext.h
@@ -0,0 +1,51 @@
+/*
+ * OpenAlContext.h
+ *
+ *  Created on: 24 Sep 2018
+ *      Author: zal
+ */
+
+#ifndef INCLUDE_ODCORE_AUDIO_OPENALCONTEXT_H_
+#define INCLUDE_ODCORE_AUDIO_OPENALCONTEXT_H_
+
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <string>
+
+#define OD_SOUND_DEFAULT_FREQUENCY 44100
+
+namespace odAudio
+{
+
+    /**
+     * @brief RAII wrapper for an OpenAL context
+     */
+    class OpenAlContext
+    {
+    public:
+
+        static const ALCint AbsoluteMaximumSources;
+
+        OpenAlContext(const char *deviceName = NULL, ALCint outputFrequency = OD_SOUND_DEFAULT_FREQUENCY);
+        OpenAlContext(const OpenAlContext &c) = delete;
+        ~OpenAlContext();
+
+        inline size_t getMaxSourceCount() const { return mMaxSources; }
+        inline uint32_t getOutputFrequency() const { return mOutputFrequency; }
+
+        void makeCurrent();
+
+
+    private:
+
+        void _doContextErrorCheck(const std::string &failmsg);
+
+        uint32_t mOutputFrequency;
+        ALCdevice *mDeviceHandle;
+        ALCcontext *mContextHandle;
+        ALCint mMaxSources;
+    };
+
+}
+
+#endif /* INCLUDE_ODCORE_AUDIO_OPENALCONTEXT_H_ */
