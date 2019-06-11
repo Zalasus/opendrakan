2053ed87e30806cfc86c511a4714a5f414ea1fac
Zalasus <niklas.weissner@gmail.com>
2019-06-11 18:04:45 +0200

    Add basic RIFF reader for creating music/DLS index

---
+++ src/odCore/audio/RiffReader.cpp
@@ -0,0 +1,132 @@
+/*
+ * RiffReader.cpp
+ *
+ *  Created on: 11 Jun 2019
+ *      Author: zal
+ */
+
+#include <odCore/audio/RiffReader.h>
+
+#include <cassert>
+
+#include <odCore/Exception.h>
+
+namespace odAudio
+{
+
+    RiffReader::RiffReader(od::DataReader reader)
+    : mReader(reader)
+    {
+        _readChunkHeader();
+
+        if(mChunkId != RIFF_CHUNK_ID)
+        {
+            throw od::Exception("Not a RIFF file");
+        }
+
+        mParentEnd = mChunkEnd;
+    }
+
+    RiffReader::RiffReader(od::DataReader reader, std::streamoff parentEnd)
+    : mReader(reader)
+    , mParentEnd(parentEnd)
+    {
+        _readChunkHeader();
+    }
+
+    bool RiffReader::hasNextChunk()
+    {
+        return mChunkEnd < mParentEnd;
+    }
+
+    bool RiffReader::isEnd()
+    {
+        return mChunkStart >= mParentEnd;
+    }
+
+    void RiffReader::nextChunk()
+    {
+        if(!hasNextChunk())
+        {
+            mChunkStart = mParentEnd;
+            mChunkEnd = mParentEnd;
+            return;
+        }
+
+        mReader.seek(mChunkEnd);
+
+        _readChunkHeader();
+    }
+
+    RiffReader RiffReader::firstSubchunk()
+    {
+        if(!mHasSubchunks)
+        {
+            throw od::Exception("Chunk has no subchunks");
+        }
+
+        mReader.seek(mChunkStart + 12);
+
+        return RiffReader(mReader, mChunkEnd);
+    }
+
+    static void _recursiveRiffPrint(std::ostream &out, odAudio::RiffReader rr, int depth)
+    {
+        while(!rr.isEnd())
+        {
+            for(int i = 0 ; i < depth; ++i)
+            {
+                out << "   |";
+            }
+
+            if(rr.hasSubchunks())
+            {
+                out << "-" << rr.getChunkId() << "/" << rr.getListId() << " (length=" << rr.getChunkLength() << ")" << std::endl;
+                _recursiveRiffPrint(out, rr.firstSubchunk(), depth+1);
+
+            }else
+            {
+                out << "-" << rr.getChunkId() << " (length=" << rr.getChunkLength() << ")" << std::endl;
+            }
+
+            rr.nextChunk();
+        }
+    }
+
+    void RiffReader::printTree(std::ostream &out)
+    {
+        _recursiveRiffPrint(out, *this, 0);
+    }
+
+    void RiffReader::_readChunkHeader()
+    {
+        mChunkStart = mReader.tell();
+
+        mChunkId = _readFourCC();
+        mReader >> mChunkLength;
+
+        mHasSubchunks = (mChunkId == LIST_CHUNK_ID || mChunkId == RIFF_CHUNK_ID);
+        if(mHasSubchunks)
+        {
+            mListId = _readFourCC();
+
+        }else
+        {
+            mListId.clear();
+        }
+
+        size_t bytesToSkip = mChunkLength + 8; // 8 header bytes not included in length
+        if(bytesToSkip & 1) ++bytesToSkip; // sizes are padded to always be even
+        mChunkEnd = mChunkStart + bytesToSkip;
+    }
+
+    std::string RiffReader::_readFourCC()
+    {
+        char c[5] = {0};
+        mReader.read(&c[0], 4);
+
+        return std::string(&c[0]);
+    }
+
+}
+
