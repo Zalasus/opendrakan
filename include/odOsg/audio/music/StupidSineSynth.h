1469e2548acb5e772eec7f60f5a2c422d9bb231f
Zalasus <niklas.weissner@gmail.com>
2019-07-15 10:59:28 +0200

    Add stupid sine synth

---
+++ include/odOsg/audio/music/StupidSineSynth.h
@@ -0,0 +1,51 @@
+/*
+ * StupidSineSynth.h
+ *
+ *  Created on: Jun 28, 2019
+ *      Author: zal
+ */
+
+#ifndef INCLUDE_ODOSG_AUDIO_MUSIC_STUPIDSINESYNTH_H_
+#define INCLUDE_ODOSG_AUDIO_MUSIC_STUPIDSINESYNTH_H_
+
+#include <vector>
+
+#include <odCore/audio/music/MidiSynth.h>
+
+namespace odOsg
+{
+
+    /**
+     * Simple MIDI synth for debugging. Synthesizes notes from a single channel as pure sine tones without any attack curves etc.
+     *
+     * This sounds horrible. Do not use in production code!
+     */
+    class StupidSineSynth : public odAudio::MidiSynth
+    {
+    public:
+
+        StupidSineSynth(uint8_t channel);
+
+        virtual void noteOn(uint8_t channel, uint8_t note, uint8_t velocity) override;
+        virtual void noteOff(uint8_t channel, uint8_t note) override;
+        virtual void controllerChange(uint8_t channel, uint8_t controller, uint8_t value) override;
+        virtual void channelPressure(uint8_t channel, uint8_t value) override;
+
+        virtual void fillInterleavedStereoBuffer(int16_t *buffer, size_t size) override;
+
+        void setChannel(uint8_t channel);
+
+
+    private:
+
+        uint8_t mChannel;
+        std::vector<bool> mNotes;
+        std::vector<double> mFrequencies;
+        double mT;
+
+    };
+
+}
+
+
+#endif /* INCLUDE_ODOSG_AUDIO_MUSIC_STUPIDSINESYNTH_H_ */
---
+++ src/odOsg/audio/music/StupidSineSynth.cpp
@@ -0,0 +1,93 @@
+/*
+ * StupidSineSynth.cpp
+ *
+ *  Created on: Jun 28, 2019
+ *      Author: zal
+ */
+
+#include <odOsg/audio/music/StupidSineSynth.h>
+
+#include <cmath>
+
+#include <odCore/Logger.h>
+
+namespace odOsg
+{
+
+    StupidSineSynth::StupidSineSynth(uint8_t channel)
+    : mChannel(channel)
+    , mT(0)
+    {
+        mNotes.resize(108-21, false);
+
+        double step = std::pow(2.0, 1.0/12.0);
+        mFrequencies.resize(108-21, 0);
+        for(size_t note = 0; note < mFrequencies.size(); ++note)
+        {
+            mFrequencies[note] = 27.5 * std::pow(step, note);
+        }
+    }
+
+    void StupidSineSynth::noteOn(uint8_t channel, uint8_t note, uint8_t velocity)
+    {
+        Logger::info() << "(note on) n=" << (uint32_t)note << " c=" << (uint32_t)channel << " v=" << (uint32_t)velocity;
+
+        if(channel == mChannel)
+        {
+            mNotes[note - 21] = true;
+        }
+    }
+
+    void StupidSineSynth::noteOff(uint8_t channel, uint8_t note)
+    {
+        Logger::info() << "(note off) n=" << (uint32_t)note << " c=" << (uint32_t)channel;
+
+        if(channel == mChannel)
+        {
+            mNotes[note - 21] = false;
+        }
+    }
+
+    void StupidSineSynth::controllerChange(uint8_t channel, uint8_t controller, uint8_t value)
+    {
+    }
+
+    void StupidSineSynth::channelPressure(uint8_t channel, uint8_t value)
+    {
+    }
+
+    void StupidSineSynth::fillInterleavedStereoBuffer(int16_t *buffer, size_t size)
+    {
+        double ampl = 0.8;
+        for(size_t i = 0; i < size/2; ++i)
+        {
+            double sample = 0;
+            size_t notes = 0;
+            for(size_t note = 0; note < mNotes.size(); ++note)
+            {
+                if(!mNotes[note]) continue;
+
+                double f = mFrequencies[note];
+                sample += std::sin(mT*2*M_PI*f);
+                notes++;
+            }
+            if(notes > 0) sample /= notes;
+            buffer[2*i] = ampl*0xffff*0.5*sample;
+            buffer[2*i+1] = buffer[2*i];
+            mT += 1.0/44100;
+        }
+    }
+
+    void StupidSineSynth::setChannel(uint8_t channel)
+    {
+        mChannel = channel;
+
+        for(size_t i = 0; i < mNotes.size(); ++i)
+        {
+            mNotes[i] = false;
+        }
+    }
+
+}
+
+
